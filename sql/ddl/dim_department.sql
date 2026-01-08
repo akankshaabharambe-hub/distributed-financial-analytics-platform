80923b0a5493112adca520291c0cabf5ebaee898
Akanksha Bharambe <akankshaabharambe@gmail.com>
2026-01-08 11:28:52 +0530

    Add department dimension table schema

--- sql/ddl/dim_department.sql
+++ sql/ddl/dim_department.sql
@@ -1,1 +1,13 @@
+-- sql/ddl/dim_department.sql
+--
+-- Department dimension table
 
+CREATE TABLE IF NOT EXISTS finance.dim_department (
+  department_id STRING NOT NULL,
+  department_name STRING NOT NULL,
+  cost_center STRING,
+  is_active BOOL DEFAULT TRUE
+)
+OPTIONS (
+  description = "Department dimension for financial reporting"
+);
